b7f1a2ded458d0bfec5835adba1f2ad2f8def70f
geirawsm <geirawsm@pm.me>
2021-12-29 23:54:12 +0100

    Added some moderation commands

--- discord_rss/__main__.py
+++ discord_rss/__main__.py
@@ -59,4 +59,49 @@ async def polse(ctx):
     'Poster det famøse "Pølse-gate"-klippet fra Tangerudbakken'
     await ctx.send('https://www.youtube.com/watch?v=geJZ3kJUqoY')
 
+
+@_config.bot.command(name='ping')
+async def ping(ctx):
+    await ctx.send(f'Pong! {round(_config.bot.latency * 1000)} ms')
+    await ctx.message.add_reaction(emoji='✅')
+
+
+@_config.bot.command(aliases=['purge', 'clear', 'cls'])
+@commands.has_permissions(manage_messages=True)
+async def prune(ctx, amount=0):
+    if amount == 0:
+        await ctx.send('Please specify the number of messages you want to delete!')
+        await ctx.message.add_reaction(emoji='❌')
+    elif amount <= 0:  # lower then 0
+        await ctx.send("The number must be bigger than 0!")
+        await ctx.message.add_reaction(emoji='❌')
+    else:
+        await ctx.message.add_reaction(emoji='✅')
+        await ctx.channel.purge(limit=amount + 1)
+
+
+@_config.bot.command()
+@commands.has_permissions(kick_members=True)  # check user permission
+async def kick(ctx, member: discord.Member, *, reason=None):
+    try:
+        await member.kick(reason=reason)
+        await ctx.send(f'{member} has been kicked!')
+        await ctx.message.add_reaction(emoji='✅')
+    except Exception as failkick:
+        await ctx.send("Failed to kick: " + str(failkick))
+        await ctx.message.add_reaction(emoji='❌')
+
+
+@_config.bot.command()
+@commands.has_permissions(ban_members=True)  # check user permission
+async def ban(ctx, member: discord.Member, *, reason=None):
+    try:
+        await member.ban(reason=reason)
+        await ctx.send(f'{member} has been banned!')
+        await ctx.message.add_reaction(emoji='✅')
+    except Exception as e:
+        await ctx.send("Failed to ban: " + str(e))
+        await ctx.message.add_reaction(emoji='❌')
+
+
 _config.bot.run(_config.TOKEN)
