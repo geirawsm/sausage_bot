87f049b6bc8e3874b1442e66d70e9612165bfd13
geirawsm <geirawsm@pm.me>
2022-09-14 14:31:28 +0200

    Converting cog functions in __main__ to class

--- sausage_bot/__main__.py
+++ sausage_bot/__main__.py
@@ -38,7 +38,7 @@ async def on_ready():
         if guild.name == _config.GUILD:
             break
     log.log('{} has connected to `{}`'.format(_config.bot.user, guild.name))
-    await load_and_clean_cogs()
+    await Cog.load_and_clean_cogs()
     if args.maintenance:
         log.log('Maintenance mode activated', color='RED')
         await _config.bot.change_presence(
@@ -56,83 +56,102 @@ async def on_ready():
         )
 
 # Cogs
-def change_status(cog_name, status):
-    cogs_status = file_io.read_json(_vars.cogs_status_file)
-    try:
-        # Change status
-        cogs_status[cog_name] = status
-        # Write changes
+class Cog:
+    'Control the cogs for the bot'
+    def change_cog_status(cog_name, status):
+        '''
+        Change a cog status in the status file
+
+        `cog_name` should be name of cog
+
+        `status` should be `enable` or `disable`
+        '''
+        accepted_status = ['enable', 'disable']
+        if not any(status == ok_status for ok_status in accepted_status):
+            log.log('This command only accept `enable` or `disable`')
+            return False
+        cogs_status = file_io.read_json(_vars.cogs_status_file)
+        try:
+            # Change status
+            cogs_status[cog_name] = status
+            # Write changes
+            file_io.write_json(_vars.cogs_status_file, cogs_status)
+            return True
+        except Exception as e:
+            log.log(_vars.COGS_CHANGE_STATUS_FAIL.format(e))
+            return False
+
+    async def load_cog(cog_name):
+        'Load a specific cog by `cog_name`'
+        await _config.bot.load_extension(
+            '{}.{}'.format(
+                _vars.COGS_REL_DIR, f'{cog_name}'
+            )
+        )
+        return
+
+    async def unload_cog(cog_name):
+        'Unload a specific cog by `cog_name`'
+        await _config.bot.unload_extension(
+            '{}.{}'.format(
+                _vars.COGS_REL_DIR, f'{cog_name}'
+            )
+        )
+        return
+
+    async def reload_cog(cog_name):
+        'Reload a specific cog by `cog_name`'
+        await _config.bot.reload_extension(
+            '{}.{}'.format(
+                _vars.COGS_REL_DIR, f'{cog_name}'
+            )
+        )
+        return
+
+    async def load_and_clean_cogs():
+        '''
+        This does three things:
+        1. Adds cogs from files that are not already present in the
+            `cogs_status` file
+        2. Removes cogs from the `cogs_status` file if a file doesn't exist
+        3. Start cogs based on status in `cogs_status` file
+        '''
+        cog_files = []
+        # Add cogs that are not present in the `cogs_status` file
+        cogs_status = file_io.read_json(_vars.cogs_status_file)
+        for filename in os.listdir(_vars.COGS_DIR):
+            if filename.endswith('.py'):
+                cog_name = filename[:-3]
+                # Add all cog names to `cog_files` for easier cleaning
+                cog_files.append(cog_name)
+                if cog_name not in cogs_status:
+                    # Added as disable
+                    log.log('Added cog {} to cogs_status file'.format(cog_name))
+                    cogs_status[cog_name] = 'disable'
+        # Clean out cogs that no longer has a file
+        log.log('Checking `cogs_status` file for non-existing cogs')
+        to_be_removed = []
+        for cog_name in cogs_status:
+            if cog_name not in cog_files:
+                log.log(f'Removing `{cog_name}`')
+                to_be_removed.append(cog_name)
+        for removal in to_be_removed:
+            cogs_status.pop(removal)
+        # Start cogs based on status
+        log.log('Checking `cogs_status` file for enabled cogs')
+        for cog_name in cogs_status:
+            if cogs_status[cog_name] == 'enable':
+                log.log('Loading cog: {}'.format(cog_name))
+                await Cog.load_cog(cog_name)
         file_io.write_json(_vars.cogs_status_file, cogs_status)
-        return True
-    except Exception as e:
-        log.log(_vars.COGS_CHANGE_STATUS_FAIL.format(e))
-        return False
 
-
-async def load_cog(cog_name):
-    await _config.bot.load_extension(
-        '{}.{}'.format(
-            _vars.COGS_REL_DIR, f'{cog_name}'
-        )
-    )
-    return
-
-
-async def unload_cog(cog_name):
-    await _config.bot.unload_extension(
-        '{}.{}'.format(
-            _vars.COGS_REL_DIR, f'{cog_name}'
-        )
-    )
-    return
-
-
-async def reload_cog(cog_name):
-    await _config.bot.reload_extension(
-        '{}.{}'.format(
-            _vars.COGS_REL_DIR, f'{cog_name}'
-        )
-    )
-    return
-
-
-async def load_and_clean_cogs():
-    cog_files = []
-    # Add cogs that are not present in the `cogs_status` file
-    cogs_status = file_io.read_json(_vars.cogs_status_file)
-    for filename in os.listdir(_vars.COGS_DIR):
-        if filename.endswith('.py'):
-            cog_name = filename[:-3]
-            # Add all cog names to `cog_files` for easier cleaning
-            cog_files.append(cog_name)
-            if cog_name not in cogs_status:
-                # Added as disable
-                log.log('Added cog {} to cogs_status file'.format(cog_name))
-                cogs_status[cog_name] = 'disable'
-    # Clean out cogs that no longer has a file
-    log.log('Checking `cogs_status` file for non-existing cogs')
-    to_be_removed = []
-    for cog_name in cogs_status:
-        if cog_name not in cog_files:
-            log.log(f'Removing `{cog_name}`')
-            to_be_removed.append(cog_name)
-    for removal in to_be_removed:
-        cogs_status.pop(removal)
-    # Start cogs based on status
-    log.log('Checking `cogs_status` file for enabled cogs')
-    for cog_name in cogs_status:
-        if cogs_status[cog_name] == 'enable':
-            log.log('Loading cog: {}'.format(cog_name))
-            await load_cog(cog_name)
-    file_io.write_json(_vars.cogs_status_file, cogs_status)
-
-
-async def reload_all_cogs():
-    cogs_status = file_io.read_json(_vars.cogs_status_file)
-    for cog_name in cogs_status:
-        if cogs_status[cog_name] == 'enable':
-            log.log('Reloading cog: {}'.format(cog_name))
-            await reload_cog(cog_name)
+    async def reload_all_cogs():
+        'Reload all cogs which is already enabled'
+        cogs_status = file_io.read_json(_vars.cogs_status_file)
+        for cog_name in cogs_status:
+            if cogs_status[cog_name] == 'enable':
+                log.log('Reloading cog: {}'.format(cog_name))
+                await Cog.reload_cog(cog_name)
 
 
 # Commands
@@ -247,16 +266,16 @@ async def cog(ctx, cmd_in=None, cog_name=None):
 
     if cmd_in == 'enable':
         # Start Cog
-        await load_cog(cog_name)
-        change_status(cog_name, 'enable')
+        await Cog.load_cog(cog_name)
+        Cog.change_cog_status(cog_name, 'enable')
         await ctx.send(
             _vars.COGS_ENABLED.format(cog_name)
         )
         return True
     elif cmd_in == 'disable':
         # Stop cog
-        await unload_cog(cog_name)
-        change_status(cog_name, 'disable')
+        await Cog.unload_cog(cog_name)
+        Cog.change_cog_status(cog_name, 'disable')
         await ctx.send(
             _vars.COGS_DISABLED.format(cog_name)
         )
