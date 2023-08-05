c534aafac7ddc51f663a6d50eee0e87d749f7fda
geirawsm <geirawsm@pm.me>
2023-08-05 09:23:36 +0200

    Improved file_io_test

--- sausage_bot/test/file_io_test.py
+++ sausage_bot/test/file_io_test.py
@@ -7,7 +7,7 @@ from sausage_bot.util import envs
 
 
 def test_write_file_input():
-    with mock.patch('sausage_bot.funcs.file_io.open') as _file:
+    with mock.patch('sausage_bot.util.file_io.open'):
         # Perfect example
         assert file_io.write_file(
             'sausage_bot/test/out/testfile', 'test') is True
@@ -23,12 +23,12 @@ def test_import_file_as_list(mocker):
     mocker_file_size.return_value.ok = 500
     # Valid list
     with mock.patch(
-        'sausage_bot.funcs.file_io.open',
+        'sausage_bot.util.file_io.open',
             new=mock.mock_open(read_data="['one', 'two']")) as _file:
         assert file_io.import_file_as_list(_file) == ['one', 'two']
     # Invalid list
     with mock.patch(
-        'sausage_bot.funcs.file_io.open',
+        'sausage_bot.util.file_io.open',
             new=mock.mock_open(read_data="['one', 'two")) as _file:
         pytest.raises(SyntaxError)
 
@@ -40,7 +40,7 @@ def test_add_to_list(mocker):
     mocker_file_size.return_value.ok = 500
     # Normal text should be added to the list
     with mock.patch(
-        'sausage_bot.funcs.file_io.open',
+        'sausage_bot.util.file_io.open',
         new=mock.mock_open(read_data="['one', 'two']")
     ) as _file:
         assert file_io.add_to_list(_file, 'three')\
@@ -53,10 +53,10 @@ def test_read_json(mocker):
     mocker_file_size = mocker.patch('os.stat')
     mocker_file_size.return_value.ok = 500
     # Valid dict
-    with mock.patch('sausage_bot.funcs.file_io.open', new=mock.mock_open(read_data='{"one": "", "two": []}')) as _file:
+    with mock.patch('sausage_bot.util.file_io.open', new=mock.mock_open(read_data='{"one": "", "two": []}')) as _file:
         assert file_io.read_json(_file) == {'one': '', 'two': []}
     # Invalid dict
-    with mock.patch('sausage_bot.funcs.file_io.open', new=mock.mock_open(read_data='{"one": "", "two": [')) as _file:
+    with mock.patch('sausage_bot.util.file_io.open', new=mock.mock_open(read_data='{"one": "", "two": [')) as _file:
         assert file_io.read_json(_file) is None
 
 
@@ -70,5 +70,9 @@ def test_file_size():
 def test_check_similarity():
     similar1 = ('tested1', 'tested1')
     dissimilar1 = ('tested1', 'tested2')
-    assert file_io.check_similarity(similar1[0], similar1[1]) is True
+    similar_list = ['tested2', 'tested3', 'tested1']
+    dissimilar_list = ['tested2', 'tested3']
+    assert file_io.check_similarity(similar1[0], similar1[1]) == 'tested1'
     assert file_io.check_similarity(dissimilar1[0], dissimilar1[1]) is False
+    assert file_io.check_similarity(similar1[0], similar_list) == 'tested1'
+    assert file_io.check_similarity(similar1[0], dissimilar_list) is False
