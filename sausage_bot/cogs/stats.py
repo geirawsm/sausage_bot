401a18a9aee11ca7fc6d17c0e9882f2ed6a78c45
geirawsm <geirawsm@pm.me>
2023-08-12 10:37:09 +0200

    Improved `stats` with filtering and tabulate

--- sausage_bot/cogs/stats.py
+++ sausage_bot/cogs/stats.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 import os
 from discord.ext import commands, tasks
+from tabulate import tabulate
 
 from sausage_bot.util import envs, datetime_handling, file_io, config
 from sausage_bot.util import discord_commands
@@ -52,39 +53,60 @@ class Stats(commands.Cog):
         The channel is defined in the .env file (stats_channel).
         '''
         def tabify(
-            dict_in: dict, _key: str, _item: str, prefix='', suffix='',
-            split='', filter_away: bool = False, sort: bool = False
+            dict_in: dict,
+            filter_away: list = config.env.list(
+                'STATS_HIDE_ROLES', default=[]
+            ),
+            sort_abc: bool = config.env.bool(
+                'STATS_SORT_ROLES_ABC', default=False
+            ),
+            sort_321: bool = config.env.bool(
+                'STATS_SORT_ROLES_321', default=False
+            )
         ):
             filter_away_lower = [x.lower() for x in filter_away]
+            log.debug(f'Using this for filter:\n{filter_away_lower}')
             text_out = ''
-            _key_len = 0
-            _item_len = 0
             if isinstance(dict_in, dict):
+                if sort_abc:
+                    dict_in = dict(sorted(dict_in.items(), key=str.lower))
+                    log.debug(f'Sorting roles alphabetically: {list(dict_in)}')
+                if sort_321:
+                    dict_in = dict(sorted(
+                        dict_in.items(), key=lambda x: x[1]['members'],
+                        reverse=True
+                    ))
+                    log.debug(
+                        f'Sorting roles by number of members: {dict_in}'
+                    )
+                # Tabulate the output
+                dict_out = {
+                    'name': [],
+                    'members': []
+                }
                 for role in dict_in:
-                    _key_count = len(str(dict_in[role][_key])) + 2
-                    if _key_count > _key_len:
-                        _key_len = _key_count
-                    _item_count = len(str(dict_in[role][_item])) + 2
-                    if _item_count > _item_len:
-                        _item_len = _item_count
-                if role.lower() not in filter_away_lower:
-                    _k = dict_in[role][_key]
-                    _i = dict_in[role][_item]
-                    text_out += f'{prefix}{_k:<{_key_len}}{split}'\
-                        f'{_i:<{_item_len}}{suffix}'
-                    if dict_in[role][_key] != dict_in.keys():
-                        text_out += '\n'
-            log.debug(f'Returning:```{text_out}```')
-            return text_out
+                    if role.lower() not in filter_away_lower and\
+                            role.lower() != '@everyone':
+                        dict_out['name'].append(dict_in[role]['name'])
+                        dict_out['members'].append(dict_in[role]['members'])
+                text_out = '{}'.format(
+                    tabulate(
+                        dict_out, headers=['Rolle', 'Bruker'], numalign='center'
+                    )
+                )
+                log.debug(f'Returning: {text_out}')
+                return text_out
+            else:
+                log.more('`dict_in` is not a dict. Check the input.')
 
         log.log('Starting `update_stats`')
         stats_channel = config.env('STATS_CHANNEL', default='stats')
         stats_log = file_io.read_json(envs.stats_logs_file)
-        # Get server members as of now
-        members = get_role_numbers()
-        _codebase = get_stats_codebase()
-        lines_in_codebase = _codebase['total_lines']
-        files_in_codebase = _codebase['total_files']
+        if config.env('STATS_CODE', default=True):
+            # Get stats about the code
+            _codebase = get_stats_codebase()
+            lines_in_codebase = _codebase['total_lines']
+            files_in_codebase = _codebase['total_files']
         _y = datetime_handling.get_dt('year')
         _m = datetime_handling.get_dt('month')
         _d = datetime_handling.get_dt('day')
@@ -93,32 +115,38 @@ class Stats(commands.Cog):
         if _m not in stats_log[_y]:
             stats_log[_y][_m] = {}
         if _d not in stats_log[_y][_m]:
-            stats_log[_y][_m][_d] = {
-                'members': {
-                    'total': members['member_count'],
-                    'roles': members['roles']
-                },
-                'files_in_codebase': files_in_codebase,
-                'lines_in_codebase': lines_in_codebase
+            stats_log[_y][_m][_d] = {}
+        if config.env('STATS_ROLES', default=True):
+            # Get server members
+            members = get_role_numbers()
+            stats_log[_y][_m][_d]['members'] = {
+                'total': members['member_count'],
+                'roles': members['roles']
             }
+        if config.env.bool('STATS_CODE', default=True):
+            # Get info about the codebase
+            stats_log[_y][_m][_d]['files_in_codebase'] = files_in_codebase
+            stats_log[_y][_m][_d]['lines_in_codebase'] = lines_in_codebase
         # Update the stats-msg
-        total_members = members['member_count']
-        roles_members = tabify(
-            members['roles'], 'name', 'members', prefix='  ', split=': ',
-            filter_away=config.env.list('STATS_HIDE_ROLES')
-        )
+        if config.env.bool('STATS_ROLES', default=True):
+            total_members = members['member_count']
+            roles_members = tabify(
+                members['roles'], 'name', 'members', prefix='  ', split=': ',
+            )
         dt_log = datetime_handling.get_dt('datetimefull')
-        stats_msg = f'> Medlemmer\n```'\
-            f'Antall medlemmer: {total_members}\n'\
-            f'Antall per rolle:\n{roles_members}```\n'\
-            f'> Kodebase\n```'\
-            f'Antall filer med kode: {files_in_codebase}\n'\
-            f'Antall linjer med kode: {lines_in_codebase}```\n'\
-            f'```(Serverstats sist oppdatert: {dt_log})'\
-            f'```\n'
+        stats_msg = ''
+        if config.env.bool('STATS_ROLES', default=True):
+            stats_msg += f'> Medlemmer\n```'\
+                f'Antall medlemmer: {total_members}\n\n'\
+                f'{roles_members}```\n'
+        if config.env.bool('STATS_CODE', default=True):
+            stats_msg += f'> Kodebase\n```'\
+                f'Antall filer med kode: {files_in_codebase}\n'\
+                f'Antall linjer med kode: {lines_in_codebase}```\n'
+        stats_msg += f'```(Serverstats sist oppdatert: {dt_log})```\n'
         log.log_more(
             f'Trying to post stats to `{stats_channel}`:\n'
-            '{stats_msg}'
+            f'{stats_msg}'
         )
         await discord_commands.update_stats_post(
             stats_msg, stats_channel
