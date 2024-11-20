ef6ed125e19f21e6fd897485632c4e5bcb6b8895
geirawsm <geirawsm@pm.me>
2024-11-20 01:07:21 +0100

    Testing spotipy does notwork

--- sausage_bot/test/net_io_test.py
+++ sausage_bot/test/net_io_test.py
@@ -7,20 +7,6 @@ from pendulum.datetime import DateTime
 from sausage_bot.util import net_io
 
 
-async def test_check_spotify_podcast_url():
-    good_url1 = 'https://open.spotify.com/show/7CJKujLFxINFP3G4zns6nw?si=QM4HOzU5RlGuDOEOVLOtCQ'
-    good_url2 = 'https://open.spotify.com/show/47qpUkCsSOLCkda3oE710f?si=5fb79d4d90ea43bd'
-    # This lacks the `?si=...` in the end, but is still accepted
-    good_url3 = 'https://open.spotify.com/show/47qpUkCsSOLCkda3oE710f'
-    # Obviously  not a spotify link
-    bad_url1 = 'https://www.youtube.com'
-
-    assert type(await net_io.check_spotify_podcast(good_url1)) is int
-    assert type(await net_io.check_spotify_podcast(good_url2)) is int
-    assert type(await net_io.check_spotify_podcast(good_url3)) is int
-    assert await net_io.check_spotify_podcast(bad_url1) is False
-
-
 async def test_make_event_start_stop():
     date_yes, time_yes = ('17.05.2022', '21:00')
     date_yes, time_no = ('17.05.2022', '671:00')
