b77fa17ce4a7f2e02f0bea3c25a7527cfb280e8e
geirawsm <geirawsm@pm.me>
2025-05-03 23:37:27 +0200

    Fixing `logger` in files

--- sausage_bot/util/logger.py
+++ sausage_bot/util/logger.py
@@ -2,8 +2,72 @@
 # -*- coding: UTF-8 -*-
 import logging
 from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
+import os
+import stat
+import json
 
 from sausage_bot.util import envs
+
+
+def file_size(filename):
+    '''
+    Checks the file size of a file. If it can't find the file it will
+    return False
+    '''
+    try:
+        _stats = os.stat(filename, follow_symlinks=True)
+        return _stats[stat.ST_SIZE]
+    except FileNotFoundError:
+        return False
+
+
+def ensure_folder(folder_path: str):
+    '''
+    Create folders in `folder_path` if it doesn't exist
+    '''
+    folder_path = str(folder_path)
+    # Make the folders if necessary
+    if not os.path.exists(folder_path):
+        _dirs = str(folder_path).split(os.sep)
+        _path = ''
+        for _dir in _dirs:
+            _path += '{}/'.format(_dir)
+        Path(_path).mkdir(parents=True, exist_ok=True)
+
+
+def ensure_file(file_path_in: str, file_template=False):
+    '''
+    Create file `file_path_in` if it doesn't exist and include the
+    `file_template` if provided.
+    '''
+    full_file_path = str(file_path_in).split(os.sep)
+    folder_path = '/'.join(full_file_path[0:-1])
+    folder_path += '/'
+    file_name = full_file_path[-1]
+    # Make the folders if necessary
+    if not os.path.exists(file_path_in):
+        ensure_folder(folder_path)
+    # Ooooh, this is a scary one. Don't overwrite the file unless it's empty
+    # Create the file if it doesn't exist
+    if not file_size(file_path_in):
+        if file_name.split('.')[-1] == 'json':
+            if file_template:
+                write_json(file_path_in, file_template)
+            else:
+                write_json(file_path_in, {})
+        else:
+            with open(file_path_in, 'w+') as fout:
+                if file_template:
+                    fout.write(file_template)
+                else:
+                    fout.write('')
+
+
+def write_json(json_file, json_out):
+    'Write `json_out` to `json file`'
+    with open(json_file, 'w') as write_file:
+        json.dump(json_out, write_file, indent=4, sort_keys=True)
 
 
 class ColorFormatter(logging.Formatter):
@@ -50,6 +114,7 @@ def configure_logging(
     logger.addHandler(console_handler)
 
     if to_file:
+        ensure_file(envs.LOG_DIR / 'bot.log')
         file_handler = TimedRotatingFileHandler(
             filename=envs.LOG_DIR / 'bot.log',
             when="midnight",
