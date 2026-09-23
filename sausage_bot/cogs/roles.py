11239b4b0bb598539e2cb938cee04fc0969f4332
geirawsm <geirawsm@pm.me>
2026-09-23 23:30:49 +0200

    roles: Make message_text not mandatory

--- sausage_bot/cogs/roles.py
+++ sausage_bot/cogs/roles.py
@@ -1429,7 +1429,7 @@ class Autoroles(commands.Cog):
         self,
         interaction: discord.Interaction,
         msg_name: str,
-        message_text: str,
+        message_text: str = None,
         order: int,
         channel: discord.TextChannel,
         roles: str,
@@ -1501,10 +1501,11 @@ class Autoroles(commands.Cog):
             embed_json = None
         else:
             embed_json = discord.Embed.from_dict({"description": desc_out})
+        content = ""
         if header:
-            content = f"## {header}\n{message_text}"
-        else:
-            content = message_text
+            content = f"## {header}"
+        if message_text:
+            content = f"{content}\n{message_text}"
         # Post the reaction message
         reaction_msg = await channel.send(content=content, embed=embed_json)
         # This uuid ties the message to its reaction roles for as long as
